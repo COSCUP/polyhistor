a42f882b8e180fe673d39b09f4f17a858b3f79a1
kt-cheng <katy881115@gmail.com>
2024-05-27 23:33:53 +0800

    feat: Add file detection loader and merge with splitter

---
+++ vectorDB/create_collection.py
@@ -0,0 +1,28 @@
+from classes.vectorDB import VectorDB
+
+def main():
+    db = VectorDB(host="http://localhost:6333")
+    COLLECTION_NAME = "test"
+
+    collection_flag = True
+    recreation_Flag = True
+
+    # 是否重建collection
+    if recreation_Flag:
+        print(f"Create collection-{COLLECTION_NAME}")
+        db.recreate_collection(COLLECTION_NAME)
+    else:
+        # 確認collection是否存在
+        collections = db.client.get_collections().collections
+        for collection in collections:
+            if collection.name == COLLECTION_NAME:
+                collection_flag = False
+                collection_info = db.client.get_collection(collection.name)
+                print(f"collection-{collection.name} 存在")
+                print(f"---collection info---\n{collection_info}")
+        if collection_flag == True:
+            print(f"Create collection-{COLLECTION_NAME}")
+            db.create_collection(COLLECTION_NAME)
+
+if __name__ == "__main__":
+    main()
---
+++ vectorDB/utils.py
@@ -0,0 +1,78 @@
+import hashlib
+
+from qdrant_client.http import models
+from qdrant_client import QdrantClient
+
+
+def compute_file_hash(file_path: str):
+    """Compute the SHA-256 hash of the given file.
+
+    Args:
+        file_path (str): Path to the file to hash.
+
+    Returns:
+        str: The computed hash value of the file.
+    """
+    hash_algo = hashlib.sha256()
+    try:
+        with open(file_path, "rb") as f:
+            while chunk := f.read(8192):
+                hash_algo.update(chunk)
+    except IOError as e:
+        print(f"Error reading file {file_path}: {e}")
+    return hash_algo.hexdigest()
+
+
+def retrieve_file_record(client: QdrantClient, file_path: str):
+    """Retrieve the record for a file from Qdrant.
+
+    Args:
+        client (QdrantClient): Qdrant client instance.
+        file_path (str): Path to the file to retrieve metadata for.
+
+    Returns:
+        list: Record of the file including its metadata.
+    """
+    try:
+        result, _ = client.scroll(
+            collection_name="test",
+            scroll_filter=models.Filter(
+                should=[
+                    models.FieldCondition(
+                        key="metadata.source", match=models.MatchValue(value=file_path)
+                    ),
+                ],
+            ),
+        )
+    except Exception as e:
+        print(f"Error retrieving file record for {file_path} in Qdrant: {e}")
+    return result
+
+
+def delete_file_record(client: QdrantClient, file_path: str):
+    """Delete the record for a file from Qdrant.
+
+    Args:
+        client (QdrantClient): Qdrant client instance.
+        file_path (str): Path to the file to delete metadata for.
+
+    Returns:
+        None
+    """
+    try:
+        client.delete(
+            collection_name="test",
+            points_selector=models.FilterSelector(
+                filter=models.Filter(
+                    must=[
+                        models.FieldCondition(
+                            key="metadata.source",
+                            match=models.MatchValue(value=file_path),
+                        ),
+                    ],
+                )
+            ),
+        )
+    except Exception as e:
+        print(f"Error deleting file record for {file_path} in Qdrant: {e}")
+
