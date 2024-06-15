94aa37cd55b722e91103bb4466bec12159d8a338
jeff3071 <jeff563715@gmail.com>
2024-06-15 13:59:37 +0800

    Feat/data preprocess (#8)
    * imporve data preprocess, implement multi query retriever
    
    * adjust README.md
    
    * ignore qdrant_storage
    
    * ignore qdrant
    
    * update

--- vectorDB/read_docs.py
+++ vectorDB/read_docs.py
@@ -1,14 +1,12 @@
 import os
-from dotenv import load_dotenv
 from getpass import getpass
 
-from langchain_community.embeddings import OllamaEmbeddings
-
-from qdrant_client import QdrantClient
-
-from classes.vector_db import VectorDB
-from classes.text_splitter import TextSplitter
 from classes.document_loader import DocumentLoader
+from classes.text_splitter import TextSplitter
+from classes.vector_db import VectorDB
+from dotenv import load_dotenv
+from langchain_community.embeddings import OllamaEmbeddings
+from qdrant_client import QdrantClient
 
 load_dotenv()
 
@@ -59,9 +57,7 @@ def main():
             text = split_doc.page_content
             payload = split_doc.metadata
             payload["content"] = text
-            payload["metadata"] = DocumentLoader.get_metadata(
-                doc, documentLoaderConfig["name"]
-            )
+            payload["metadata"] = DocumentLoader.get_metadata(doc, documentLoaderConfig["name"])
             vector = embedding.embed_query(text=text)
             data = {"vector": vector, "payload": payload}
 
