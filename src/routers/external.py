52863285da1382ef32f009c962c99d843bbbee43
irischen <chenluchieh@gmail.com>
2024-07-11 22:08:24 +0800

    refactor: Modify response format

--- src/routers/external.py
+++ src/routers/external.py
@@ -92,11 +92,23 @@ async def chatbot(user_name: str = Form(...), text: str = Form(...), response_ur
 
     data = Query(query=text.lower())
     contents = await askAPI(data)
-
-    ans = "Question: " + text + "\n\n" + "Answer: " + "\n" + contents + "\n\n" + "如果有什麼問題可以直接留言給我們，如果覺得答案不符合需求，也歡迎直接在留言處提供文件並標注 @jefflu 或 @jimmy_hong 或 @irischen"
-
     gc.collect()
-    return JSONResponse(content={"response_type": "in_channel", "text": ans})
+
+    return JSONResponse(
+        content={
+            "response_type": "in_channel",
+            "text": f"Hi @{user_name}",
+            "attachments": [
+                {
+                    "fields": [
+                        {"short": False, "title": "Question", "value": text},
+                        {"short": False, "title": "Answer", "value": contents},
+                        {"short": False, "title": " ", "value": "如果有什麼問題可以直接留言給我們，如果覺得答案不符合需求，也歡迎直接在留言處提供文件並標注 @jefflu 或 @jimmy_hong 或 @irischen"},
+                    ]
+                }
+            ],
+        }
+    )
 
 
 async def send_wait_response(response_url: str):
